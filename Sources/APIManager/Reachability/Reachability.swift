c32d81a8c89a27f4565442fe84a15e9bf9b056db
Pinal Prajapati <pprajapati@aecordigital.com>
2021-11-15 12:17:09 +0530

    bug fixing

--- Sources/APIManager/Reachability/Reachability.swift
+++ Sources/APIManager/Reachability/Reachability.swift
@@ -9,28 +9,22 @@ import Foundation
 import SystemConfiguration
 
 public class Reachability {
-   
-    private init() { }
     
-    public static func isConnectedToNetwork() -> Bool {
-        
-        var zeroAddress = sockaddr_in()
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
         zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
         zeroAddress.sin_family = sa_family_t(AF_INET)
+        
         guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
-            
             $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
-                
                 SCNetworkReachabilityCreateWithAddress(nil, $0)
-                
             }
-            
         }) else {
-            
             return false
         }
-        var flags = SCNetworkReachabilityFlags()
-        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+        
+        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) == false {
             return false
         }
         let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
