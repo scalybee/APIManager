08e70505d561302c7e2c315399b7dc7ad5926348
Pinal Prajapati <pprajapati@aecordigital.com>
2022-02-18 10:19:41 +0530

    to delay no internett response as response is so fast that user is not able to see loader.

--- Sources/APIManager/APIManager.swift
+++ Sources/APIManager/APIManager.swift
@@ -32,7 +32,9 @@ public class APIManager: NSObject {
     public func request<T:Codable>(_ endpoint : String, httpMethod : APIHTTPMethod, header: [String:String]?, param:[String: Any]? = nil, requestTimeout: TimeInterval = 60, completion : @escaping (Int,Result<T, Error>) -> Void){
         
         guard Reachability.isConnectedToNetwork() == true else {
-            completion(APIManagerErrors.internetOffline.statusCode,.failure(APIManagerErrors.internetOffline))
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(300)) {
+                completion(APIManagerErrors.internetOffline.statusCode,.failure(APIManagerErrors.internetOffline))
+            }
             return
         }
         
